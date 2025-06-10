4226a8ac922ddb025728817726386a7e53676de5
Yasna askari <yasna9303@gmail.com>
2025-06-10 20:56:37 +0330

    Create variables.cpp

---
+++ variables/variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // دروووود! اینجا میخوایم با متغیرها آشنا شیم
+    // متغیر یعنی یه جعبه تو حافظه که می‌تونی توش اطلاعات ذخیره کنی
+    // مثلاً عدد، متن، و غیره
+
+    int age = 25;      // عدد صحیح
+    double weight = 68.5; // عدد اعشاری
+    char grade = 'A';  // یه کاراکتر
+    string name = "Ali"; // یه رشته (متن)
+
+    cout << "Esme man " << name << " hast." << endl;
+    cout << "Senem " << age << " sale." << endl;
+    cout << "Vaznam " << weight << " kg hast." << endl;
+    cout << "Nemtan " << grade << " hast." << endl;
+
+    return 0;
+}
