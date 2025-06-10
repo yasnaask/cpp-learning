62859572fce1be76521772fd0476481518fd9b01
Yasna askari <yasna9303@gmail.com>
2025-06-10 21:38:16 +0330

    Create loops.cpp

---
+++ loops/loops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // درود رفیق!  
+    // اینجا میخوایم با حلقه‌ها آشنا بشیم.  
+    // حلقه یعنی کاری رو چند بار تکرار کنیم بدون اینکه هر بار کد رو دوباره بنویسیم.
+
+    cout << "Az 1 ta 5 print mikonim:" << endl;
+
+    for (int i = 1; i <= 5; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
