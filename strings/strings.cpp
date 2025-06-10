451bfefb0a1ecb5d52d030a062d376b696f60dd0
Yasna askari <yasna9303@gmail.com>
2025-06-10 22:08:10 +0330

    Create strings.cpp

---
+++ strings/strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    // اینجا با رشته‌ها (strings) آشنا می‌شیم.  
+    // رشته یعنی یه دنباله از کاراکترها، مثل اسامی یا جملات.
+
+    string esm = "Yasna";
+    string payam = "Salam, halet khobe?";
+
+    cout << "Esm: " << esm << endl;
+    cout << "Payam: " << payam << endl;
+
+    return 0;
+}
